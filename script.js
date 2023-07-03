cf3d2d91e17edfe13c29674c5e6243ae5f0f6dc8
Amanda <amanda_tos@hotmail.com>
2023-07-02 22:29:02 -0300

    primeiro commit

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 function contar() {
-    let inicio = document.getElementById('txti')
+    let ini = document.getElementById('txti')
     let fim = document.getElementById('txtf')
     let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
 
-    if(inicio.value.length == 0 || fim.value.length== 0 || passo.value.lenght == 0) {
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.lenght == 0) {
         window.alert('[Erro] Faltam dados')
+    } else {
+        res.innerHTML = 'Contando:'
     }
+    
 }
