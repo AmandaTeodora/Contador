1221629ab23d06ba61dca24d44b30203ccf4d7d8
Amanda <amanda_tos@hotmail.com>
2023-07-17 10:25:46 -0300

    PRIMEIRO COMMIT

--- script.js
+++ script.js
@@ -11,11 +11,18 @@ function contar() {
         let i = Number (ini.value)
         let f = Number (fim.value)
         let p = Number (passo.value)
-
-        for(let c = i; c <= f; c += p) {
-            res.innerHTML +=  `${c}`
+        if (i < f ) {
+            //contagem crescente
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1f449}`
+            }
+        } else {
+            //contagem regressiva
+            for(let c = i; c <= f; c -= p) {
+            res.innerHTML +=  ` ${c} \u{1f449}`
+            }
         }
-    
+        res.innerHTML += `\u{1f3c1}`
     }
     
 }
